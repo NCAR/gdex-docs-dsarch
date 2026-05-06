86a1b8de58934e33b5f7f9f0f1e8edd24f9b4d48
Zaihua Ji <zji@ucar.edu>
2026-05-06 15:28:41 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,8 @@ project = 'DSARCH Guide'
 copyright = '2026, GDEX@NCAR, https://gdex.ucar.edu/'
 author = 'Zaihua Ji'
 
-release = '0.6'
-version = '1.0.6'
+release = '0.7'
+version = '1.0.7'
 
 # -- General configuration
 
